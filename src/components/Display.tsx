9de9dc2a2930fabcec25b87d87b879fe00ce2d57
Максим <fava7286@gmail.com>
2025-01-21 16:22:12 +0300

    добавлена функция для добавления новой задачи

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -12,13 +12,34 @@ const Display = () => {
         {id: 1, taskText: 'Example2', isDone: true},
         {id: 2, taskText: 'Example3', isDone: false}
     ]);
+    const [taskText, setTaskText] = useState<string>('');
+
+    const handleClickAddTask  = () => {
+        setTaskText('');
+        
+        setDataTasks(
+            [
+              ...dataTasks,
+              {'id': Date.now(), 'taskText': taskText, isDone: false}
+            ]
+          );
+    };
+
+    const handleChangeTaskText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskText(e.target.value);
+    };
 
     return (
         <>
             <Header />
             <VisualEffect>
                 <Main>
-                    <Input />
+                    <Input 
+                        taskText={taskText}
+                        onChangeTaskText={handleChangeTaskText}
+                        onClickAddNewTask={handleClickAddTask}
+
+                    />
                     <ListTasks 
                         dataTasks={dataTasks}
                     />
@@ -39,4 +60,5 @@ const VisualEffect = styled.div`
     width: 100%;
     max-width: 30rem;
     box-shadow: 0 0.5rem 0.5rem 0.5rem rgba(0, 0, 0, 0.1), 0 0.2rem 0.4rem -0.1rem rgba(0, 0, 0, 0.06);
+    overflow: hidden;
 `;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = () => {
+const Input = ({ taskText, onChangeTaskText, onClickAddNewTask }: InputProps) => {
 
     return (
         <Container>
             <HideTasksListButton>
                 &lsaquo;
             </HideTasksListButton>
-            <InputTaskName type="text" placeholder="Что должно быть сделано?" />
+            <InputTaskName 
+                type="text"
+                value={taskText}
+                onChange={onChangeTaskText}
+                placeholder="Что должно быть сделано?" 
+            />
+            <AddNewTask onClick={onClickAddNewTask}>
+                +
+            </AddNewTask>
         </Container>
     )
 };
 
 export default Input;
 
+interface InputProps {
+    taskText: string;
+    onChangeTaskText: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onClickAddNewTask: () => void;
+};
+
 const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
-    height: 3rem;
+    height: 3.5rem;
     border-bottom: 0.01rem solid gray;
 `;
 
@@ -28,7 +42,7 @@ const HideTasksListButton = styled.button`
     font-size: 2.2rem;
     opacity: 0.5;
     background-color: white;
-    width: 3.2rem;
+    width: 4rem;
     height: 100%;
     transform: rotate(270deg);
 `;
@@ -39,5 +53,12 @@ const InputTaskName = styled.input`
     height: 100%;
     width: 100%;
     border: none;
-    padding-left: 0.8rem;
+    padding: 0.8rem;
+    z-index: 1;
+`;
+
+const AddNewTask = styled.button`
+    width: 5rem;
+    height: 100%;
+    font-size: 1.5rem;
 `;
